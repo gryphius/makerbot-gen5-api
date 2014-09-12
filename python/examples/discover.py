63e6c4fe1190b312f5f4782ed71dc0fcff8e4d83
Oli Schacher <oli@wgwh.ch>
2014-09-12 21:44:27 +0200

    discover example script

---
+++ python/examples/discover.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import makerbotapi
+import sys
+
+if __name__ == '__main__':
+    print "searching for 5th gen makerbot in your network..."
+    result = makerbotapi.makerbotapi.discover()
+    if len(result) == 0:
+        print "no 5th gen makerbot found :("
+        sys.exit(1)
+
+    for ip, machine_name, serial in result:
+        print "Found '%s' at %s , sn=%s" % (machine_name, ip, serial)
