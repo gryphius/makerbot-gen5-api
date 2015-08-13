43b75a315c5a72c1947b920e509a12dc054bb808
Brandon Pomeroy <pomeroyb@uw.edu>
2015-08-12 17:53:18 -0700

    update Config_example.py

--- python/examples/config_example.py
+++ python/examples/config_example.py
@@ -20,5 +20,18 @@ if __name__ == '__main__':
     bot[0] = '10.1.10.99'
     config.addBot(bot)
     
+    #All bots require an authorization code before allowing access. You can store the code in you config file if you'd like.
+    
+    #First we have to explicitly state that we are ok with saving the Auth code. 
+    #The first argument is the serial number of bot, and the second arg is saying that we have permission to save.
+    config.setAuthCodeSavePermission(bot[2], True)
+    
+    # Now we can save an authentication code to the bot.
+    config.saveAuthCode(bot[2], 'SampleAuthCode')
+    
+    # Let's check that the Auth Code actually got inputted.
+    print config.getBotInfo(bot[2])
+    
+    
     #Lastly, we'll save the config
     config.save()
