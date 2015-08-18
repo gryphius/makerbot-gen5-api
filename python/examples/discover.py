f76876a356d96195f85fa4f1472525aa22402853
gryphius <oli@wgwh.ch>
2015-08-18 07:46:35 +0200

    print full bot info in search results

--- python/examples/discover.py
+++ python/examples/discover.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == '__main__':
 
     #Set up vars
-    knownBotIps = []
+    knownBots = {} #key: the ip of the bot, value: tuple (bot name, serial)
     searching = True
     count = 0
     sleepAmount = 0.25
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         #discover() takes three args: The sockets that we created earlier, and a list of known bot ip addresses.
         #   The third arg is optional, and is the amount of time discover() will sleep after it requests a response.
         #   If you don't specify a sleep amount, it defaults to 1 second.
-        result = makerbotapi.makerbotapi.discover(sockets, knownBotIps, sleepAmount)
+        result = makerbotapi.makerbotapi.discover(sockets, knownBots.keys(), sleepAmount)
         
         #you can do other things while searching for bots.
         print "discover is non-blocking!"
@@ -29,12 +29,12 @@ if __name__ == '__main__':
         #   Since we don't want discover() to keep finding the same bot over and over again, we add the
         #   new ip to our list of known bot ips.
         for ip, machine_name, serial in result:
-            if ip not in knownBotIps:
+            if ip not in knownBots:
                 print "Found '%s' at %s , serial number=%s" % (machine_name, ip, serial)
-                knownBotIps.append(ip)
+                knownBots[ip] = (machine_name,serial)
         
         #We can print our ip list to make sure it was added correctly
-        print knownBotIps
+        print knownBots
         
         #To prevent this from going on forever, we'll set searching to False after 10 iterations.
         count = count + 1
